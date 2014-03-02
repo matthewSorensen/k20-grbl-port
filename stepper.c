834f83cc550850ea181e6472f0ade048a23a474f
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-03-01 16:58:29 -0800

    Quick hack to fix issues with delay in st_go_idle hanging during the stepper isr

--- stepper.c
+++ stepper.c
@@ -124,7 +124,7 @@ void st_go_idle()
   if ((settings.stepper_idle_lock_time != 0xff) || bit_istrue(sys.execute,EXEC_ALARM)) {
     // Force stepper dwell to lock axes for a defined amount of time to ensure the axes come to a complete
     // stop and not drift from residual inertial forces at the end of the last movement.
-    delay(settings.stepper_idle_lock_time);
+    delay_microseconds(1000 * settings.stepper_idle_lock_time);
     if (bit_istrue(settings.flags,BITFLAG_INVERT_ST_ENABLE)) { 
       STEPPER_DISABLE_PORT(COR) = STEPPER_DISABLE_BIT;
     }else{
